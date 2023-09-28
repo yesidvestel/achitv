def04c98de5a3f5e232e9b1defefbdaeb902216d
Duber1234 <dfpesca@misena.edu.co>
2023-09-28 10:11:11 -0500

    c4

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://achitel-mydic.com/';
+$config['base_url'] = 'https://achitel-mydic.com/';
 
 /*
 |--------------------------------------------------------------------------
