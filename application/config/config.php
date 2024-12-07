08516fd7c626112f79a46a2ed68ed5552d1af9c2
Duber1234 <dfpesca@misena.edu.co>
2024-12-07 17:56:42 -0500

    cambios revertidos

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8080/achitv/';
+$config['base_url'] = 'https://achitel-mydic.com/';
 
 /*
 |--------------------------------------------------------------------------
